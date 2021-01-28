b5667352dfc34b704e7fc1c1ddbfc5f9b3161c06
leahfortier <leahfortier318@gmail.com>
2021-01-28 07:10:30 -0800

    create tables in migrate script

--- scripts/migrate-db.js
+++ scripts/migrate-db.js
@@ -1,48 +1,55 @@
-const path = require('path')
-const envPath = path.resolve(process.cwd(), '.env.local')
+const path = require("path");
+const envPath = path.resolve(process.cwd(), ".env.local");
 
-console.log({ envPath })
+console.log({ envPath });
 
-require('dotenv').config({ path: envPath })
+require("dotenv").config({ path: envPath });
 
-const mysql = require('serverless-mysql')
+const mysql = require("serverless-mysql");
 
 const db = mysql({
-  config: {
-    host: process.env.MYSQL_ENDPOINT,
-    database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-  },
-})
+    config: {
+        host: process.env.MYSQL_ENDPOINT,
+        database: process.env.MYSQL_DATABASE,
+        user: process.env.MYSQL_USERNAME,
+        password: process.env.MYSQL_PASSWORD,
+    },
+});
 
 async function query(q) {
-  try {
-    const results = await db.query(q)
-    await db.end()
-    return results
-  } catch (e) {
-    throw Error(e.message)
-  }
+    try {
+        const results = await db.query(q);
+        await db.end();
+        return results;
+    } catch (e) {
+        throw Error(e.message);
+    }
 }
 
-// Create table if does not exist
+// Create user and task tables if does not yet exist
 async function migrate() {
-  try {
-    await query(`
-      CREATE TABLE IF NOT EXISTS puppy_blog (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        date DATE NOT NULL,
-        title VARCHAR(100) NOT NULL,
-        content TEXT NOT NULL
-      )
-    `)
-    console.log('migration ran successfully')
-  } catch (e) {
-    console.error(e.message)
-    console.error('could not run migration, double check your credentials.')
-    process.exit(1)
-  }
+    try {
+        await query(`
+            CREATE TABLE IF NOT EXISTS user_data (
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                email VARCHAR(100) NOT NULL,
+                data TEXT NOT NULL
+            )`);
+
+        await query(`
+            CREATE TABLE IF NOT EXISTS task_data (
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                date DATE NOT NULL,
+                task_name VARCHAR(100) NOT NULL,
+                data TEXT
+            )`);
+
+        console.log("migration ran successfully");
+    } catch (e) {
+        console.error(e.message);
+        console.error("could not run migration, double check your credentials.");
+        process.exit(1);
+    }
 }
 
-migrate().then(() => process.exit())
+migrate().then(() => process.exit());
